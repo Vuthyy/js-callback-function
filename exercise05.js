c4792c5a0b37c04ae31652e0cb31499812c80673
Optimusthy.dev <141915285+Vuthyy@users.noreply.github.com>
2024-07-08 09:13:02 +0700

    Update exercise05.js

--- exercise05.js
+++ exercise05.js
@@ -25,7 +25,7 @@ readFile("input.txt", (data) => {
   const modifiedData1 = data + "\nFirst modification";
   writeFile("output1.txt", modifiedData1, () => {
     readFile("output1.txt", (data) => {
-      const modifiedData2 = data + "\nSecond modification";
+      const modifiedData2 = data + "\nSEcond modification";
       writeFile("output2.txt", modifiedData2, () => {
         readFile("output2.txt", (data) => {
           console.log(data);
